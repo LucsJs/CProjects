d77838178e1fcfffc233f7ebbdc6bd80b3c57d1e
LucasJs <lucasjsdasilvasousa@gmail.com>
2025-03-10 11:04:43 -0300

    Update ex2-8.c

--- ExerciciosLivro/ex2-8.c
+++ ExerciciosLivro/ex2-8.c
@@ -1,1 +1,21 @@
-sjf
+/*
+função giradir(x, n) que retorne o valor do inteiro x
+girado à direita de n posições de bit
+
+ps: assumindo que o tamanho de um int é 32 bits
+
+ex: giradir(11000001, 3);
+	resultado: 00111000
+*/
+
+#define INT_BITS 32
+
+unsigned int giradir(unsigned int, int);
+
+unsigned int giradir(unsigned int x, int n) {
+	
+	int giro = n - 32 * (n/32);
+	return (x >> giro | x << INT_BITS - giro);
+}
+
+
