709b041ad3f2bc72435062b586326154c24e2594
LucasJs <lucasjsdasilvasousa@gmail.com>
2025-03-12 13:35:43 -0300

    Update ex3-03.c

--- ExerciciosLivro/ex3-03.c
+++ ExerciciosLivro/ex3-03.c
@@ -2,31 +2,33 @@
 escreva uma funçã itos(n, s) que converte um número inteiro em uma string
 */
 
-
-#include <stdio.h>
-
-int main() {
-	
-	n = -184;
-	char num[10];
-	itos(n, s);
-	printf("%s", num);
-	
-	return 0;
-}
-
+#include <string.h>
 
 void itos(int n, char s[]) {
-	int i = 0;
+	int i = 0, inicio, fim;
+	inicio = 0;
 	if (n < 0) {
 		s[i++] = '-';
+		n *= -1;
+		inicio = 1;
 	}
-	s[i++] = n % 10;
-	n /= 10;
-	while(n != 0) {
-		s[i++] = n % 10;
+	
+	do {
+		s[i++] = (n % 10) + '0';
 		n /= 10;
+	} while (n != 0);
+	
+	s[i] = '\0'; // até aqui a função pegaria -184 e tranformaria em -481. Então invertemos
+	
+	fim = strlen(s) - 1;
+	while (inicio < fim) { // parte para inverter.
+		int aux = s[inicio];
+		s[inicio++] = s[fim];
+		s[fim--] = aux;
 	}
-	s[i] = '\0';
 	
 }
+
+
+
+
