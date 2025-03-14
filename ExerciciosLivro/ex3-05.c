3ef0a77ec42923a6bbbf168b436fcac01a1b796a
LucasJs <lucasjsdasilvasousa@gmail.com>
2025-03-14 13:49:44 -0300

    Create ex3-05.c

---
+++ ExerciciosLivro/ex3-05.c
@@ -0,0 +1,7 @@
+/*
+Escreva uma função itoa(n, s[], esp) que pôe, no vetor s de char, 
+o numero n convertido em caracteres com um número de espaços.
+se houver mais espaços que dígitos, pôr os espaços na esquerda.
+*/
+
+
